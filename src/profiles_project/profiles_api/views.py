0037bbf733a6b72555259e3c25a83d3d54be93d9
Martin Luo <martinluo22@hotmail.com>
2017-12-17 14:11:12 -0500

    Added hello world test API view

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
+from rest_framework.views import APIView
+from rest_framework.views import Response
+
+
 # Create your views here.
+
+class HelloApiView(APIView):
+    """Test Api View"""
+
+    def get(self, request, format = None):
+        """Returns a list of APIView features"""
+
+        an_apiview = [
+            'Uses HTTP methods as functions (get, post, patc, put, delete)',
+            'It is similar to a tranditional Django view',
+            'Gives you the most control over your logic',
+            'Is mapped manually to URLS'
+        ]
+
+        return Response({'message' : 'Hello', 'an_apiview': an_apiview})
