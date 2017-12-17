3a1c0f4feb1a5eabce9d946e02bede32bf54f903
Martin Luo <martinluo22@hotmail.com>
2017-12-17 15:44:14 -0500

    Create first test ViewSet and router

--- src/profiles_project/profiles_api/urls.py
+++ src/profiles_project/profiles_api/urls.py
@@ -1,7 +1,17 @@
 from django.conf.urls import url
+from django.conf.urls import include
+
+from rest_framework.routers import DefaultRouter
 
 from . import views
 
+router = DefaultRouter()
+#First param: name of api, Second param: name of viewset to assign to router,
+#Third param: base_name
+router.register('hello-viewset', views.HelloViewSet, base_name = 'hello-viewset')
+
 urlpatterns = [
     url(r'^hello-view/', views.HelloApiView.as_view()),
+    #Allows router to create url for us
+    url(r'', include(router.urls))
 ]
