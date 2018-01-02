c877f5ad0da55f4127d8f9832037c92e2249ed9c
Martin Luo <martinluo22@hotmail.com>
2018-01-02 14:30:34 -0500

    Add profile feed item model and created migration

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -64,3 +64,16 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
         """Django uses this to convert object to a string"""
 
         return self.email
+
+
+class ProfileFeedItem(models.Model):
+    """Profile status update"""
+
+    user_profile = models.ForeignKey('UserProfile', on_delete = models.CASCADE)
+    status_text = models.CharField(max_length = 255)
+    created_on = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        """Return model as string"""
+
+        return self.status_text
